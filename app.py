e3886c294b8fffa3275e25b1097e13d0418da7c2
Yonghao Zhang <zyh153588@gmail.com>
2023-12-14 15:57:01 -0500

    user profile

--- app.py
+++ app.py
@@ -13,8 +13,42 @@ lock = threading.Lock()
 
 @app.route('/user', methods=['POST'])
 def create_user():
-    new_user = user_manager.create_user()
-    return jsonify(user_id=new_user.user_id, user_key=new_user.key)
+    content = request.json
+    username = content.get('username')
+    real_name = content.get('real_name', None)
+    avatar_icon = content.get('avatar_icon', None)
+
+    if not username:
+        abort(400, description="Username is required")
+
+    new_user = user_manager.create_user(username, real_name, avatar_icon)
+    return jsonify(user_id=new_user.user_id, user_key=new_user.key, username=new_user.username)
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user_metadata(user_id):
+    user = user_manager.get_user(user_id)
+    if not user:
+        abort(404, description="User not found")
+
+    return jsonify(user_id=user.user_id, username=user.username, real_name=user.real_name, avatar_icon=user.avatar_icon)
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+def edit_user_metadata(user_id):
+    content = request.json
+    user_key = content.get('user_key')
+    new_real_name = content.get('real_name')
+    new_avatar_icon = content.get('avatar_icon')
+
+    if not user_manager.validate_user(user_id, user_key):
+        abort(403, description="Invalid user ID or key")
+
+    user = user_manager.get_user(user_id)
+    if new_real_name:
+        user.real_name = new_real_name
+    if new_avatar_icon:
+        user.avatar_icon = new_avatar_icon
+
+    return jsonify(user_id=user.user_id, username=user.username, real_name=user.real_name, avatar_icon=user.avatar_icon)
 
 @app.route('/post', methods=['POST'])
 def create_post():
@@ -52,7 +86,13 @@ def read_post(post_id):
     with lock:
         if post_id in posts:
             post = posts[post_id]
-            return jsonify(id=post['id'], timestamp=post['timestamp'], msg=post['msg'])
+            user_id = post.get('user_id')
+            user_data = None
+            if user_id:
+                user = user_manager.get_user(user_id)
+                if user:
+                    user_data = {'user_id': user.user_id, 'username': user.username}
+            return jsonify(id=post['id'], timestamp=post['timestamp'], msg=post['msg'], user=user_data)
         else:
             abort(404, description="Post not found")
 
--- users.py
+++ users.py
@@ -1,19 +1,26 @@
 import os
 
 class User:
-    def __init__(self, user_id, key):
+    def __init__(self, user_id, key, username, real_name=None, avatar_icon=None):
         self.user_id = user_id
         self.key = key
+        self.username = username  
+        self.real_name = real_name  
+        self.avatar_icon = avatar_icon
 
 class UserManager:
     def __init__(self):
         self.users = {}
         self.user_id_counter = 0
 
-    def create_user(self):
+    def create_user(self, username, real_name, avatar_icon):
+        for user in self.users.values():
+            if user.username == username:
+                raise ValueError("Username already exists")
+
         self.user_id_counter += 1
         user_key = os.urandom(24).hex()
-        new_user = User(self.user_id_counter, user_key)
+        new_user = User(self.user_id_counter, user_key, username, real_name, avatar_icon)
         self.users[self.user_id_counter] = new_user
         return new_user
 
